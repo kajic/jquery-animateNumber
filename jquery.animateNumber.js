5e7f8921b8c86c784fdcc115f212df25202782ba
Dave Rodriguez <dave.rodriguez@hansoninc.com>
2013-09-03 16:28:52 -0400

    Added an option to format the animated number as currency

--- jquery.animateNumber.js
+++ jquery.animateNumber.js
@@ -14,79 +14,103 @@
 
 (function ($, undefined) {
 
-var defaults = {
-    duration : 5000,
-    easing: "swing",
-    animateOpacity: true,
-    intStepDecimals: 0,
-    intEndDecimals: 0,
-    floatStepDecimals: 4,
-    floatEndDecimals: 1,
-    callback: function() {}
-};
-    
-function round(number, decimals) {
-    return Math.round(number*Math.pow(10,decimals))/Math.pow(10,decimals);
-}
+	var defaults = {
+		duration : 5000,
+		easing: "swing",
+		animateOpacity: true,
+		intStepDecimals: 0,
+		intEndDecimals: 0,
+		floatStepDecimals: 4,
+		floatEndDecimals: 1,
+		format: "default",
+		currencyIndicator: "$",
+		currencyGroupSeparator: ",",
+		currencyDecimalSeparator: ".",
+		callback: function() {}
+	};
 
-function isInt(number) {
-    return /^-?[\d]+$/.test(number);
-}
+	function round(number, decimals) {
+		return Math.round(number*Math.pow(10,decimals))/Math.pow(10,decimals);
+	}
 
-$.fn.animateNumber = function(value, options, callback) {
-    if (typeof options === "function") {
-        callback = options;
-        options = {};
-    }
-    options = $.extend({}, defaults, options);
-    
-    return this.each(function () {
-        var container = $(this);
-        var initialValue = parseFloat($(this).text(), 10);
-        if (round(value, options.floatEndDecimals) == round(initialValue, options.floatEndDecimals)) {
-            return;
-        }
-        var type = container.data("type") || (isInt($(this).text()) ? "int" : "float"),
-            stepDecimals, endDecimals, 
-            defaultStepDecimals, defaultEndDecimals;
-        if (type == "int") {
-            defaultStepDecimals = options.intStepDecimals;
-            defaultEndDecimals = options.intEndDecimals;
-        } else {
-            defaultStepDecimals = options.floatStepDecimals;
-            defaultEndDecimals = options.floatEndDecimals;
-        }
-        stepDecimals = container.data("stepDecimals") || defaultStepDecimals;
-        endDecimals = container.data("endDecimals") || defaultEndDecimals;
-        
-        // animate opacity
-        if (options.animateOpacity) {
-            container.animate({opacity: 0.2}, {
-                duration: options.duration/2, 
-                easing: options.easing, 
-                complete: function() {
-                    container.animate({opacity: 1}, {
-                        duration: options.duration/2,
-                        easing: options.easing
-                    });
-                }
-            });
-        }
-        // animate number
-        $({number: initialValue}).animate({number: value}, {
-            duration: options.duration,
-            easing: options.easing, 
-            step: function() {
-                container.text(round(this.number, stepDecimals));
-            },
-            complete: function() {
-                container.text(round(this.number, endDecimals));
-                if (typeof options.callback === "function") {
-                    options.callback.call(container);
-                }
-            }
-        });
-    });
-};
+	function formatCurrency(number, options) {
+		if (!isNaN(number)) {
+			var whole = Math.floor(number);
+			return options.currencyIndicator + whole.toLocaleString() + options.currencyDecimalSeparator + (number - whole).toFixed(2).split('.')[1];
+		}
+
+	}
+
+	function isInt(number) {
+		return /^-?[\d]+$/.test(number);
+	}
+
+	$.fn.animateNumber = function(value, options, callback) {
+		if (typeof options === "function") {
+			callback = options;
+			options = {};
+		}
+		options = $.extend({}, defaults, options);
+
+		return this.each(function () {
+			var container = $(this);
+			var initialValue;
+			if (options.format === "currency") {
+				$(this).text( $(this).text().replace(options.currencyIndicator, "").replace(options.currencyGroupSeparator, "") );
+			}
+			var initialValue = parseFloat($(this).text(), 10);
+			if (round(value, options.floatEndDecimals) == round(initialValue, options.floatEndDecimals)) {
+				return;
+			}
+			var type = container.data("type") || (isInt($(this).text()) ? "int" : "float"),
+				stepDecimals, endDecimals,
+				defaultStepDecimals, defaultEndDecimals;
+			if (type == "int") {
+				defaultStepDecimals = options.intStepDecimals;
+				defaultEndDecimals = options.intEndDecimals;
+			} else {
+				defaultStepDecimals = options.floatStepDecimals;
+				defaultEndDecimals = options.floatEndDecimals;
+			}
+			stepDecimals = container.data("stepDecimals") || defaultStepDecimals;
+			endDecimals = container.data("endDecimals") || defaultEndDecimals;
+
+			// animate opacity
+			if (options.animateOpacity) {
+				container.animate({opacity: 0.2}, {
+					duration: options.duration/2,
+					easing: options.easing,
+					complete: function() {
+						container.animate({opacity: 1}, {
+							duration: options.duration/2,
+							easing: options.easing
+						});
+					}
+				});
+			}
+			// animate number
+			$({number: initialValue}).animate({number: value}, {
+				duration: options.duration,
+				easing: options.easing,
+				step: function() {
+					if (options.format === 'currency') {
+						container.text(formatCurrency(this.number, options));
+					} else {
+						container.text(round(this.number, stepDecimals));
+					}
+				},
+				complete: function() {
+					if (options.format === 'currency') {
+						container.text(formatCurrency(this.number, options));
+					} else {
+						container.text(round(this.number, endDecimals));
+					}
+					if (typeof options.callback === "function") {
+						options.callback.call(container);
+					}
+				}
+			});
+		});
+	};
 
 })( jQuery );
